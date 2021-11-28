d8b8581b5f191ec81bcbde6e462fcf2e501eec37
Joseph-RMM <utp0007544@alumno.utpuebla.edu.mx>
2021-11-27 20:25:55 -0600

    Correccion del manifest

--- index.php
+++ index.php
@@ -5,7 +5,12 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="style.css">
+    
     <title>CatFox Testers</title>
+    <link rel="manifest" href="manifest.json">
+    <!-- iOS -->
+    <link rel="apple-touch-icon" href="icon-192x192.png">
+    <meta name="apple-mobile-web-app-status-bar" content="#030cbb">
 </head>
 <body>
     <?php include_once "header.php" ?>
